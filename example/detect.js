edbd22abd65931de9b19b0e6ed768af6ec684264
Rafa Yepes <rafael.yepes.garcia@corp.badoo.com>
2015-10-23 10:02:18 +0100

    rename to amdetective-badoo and version bump

--- example/detect.js
+++ example/detect.js
@@ -1,5 +1,5 @@
 var fs = require('fs'),
-    amdetective = require('./index.js');
+    amdetective = require('../index.js');
 
 console.log('Reading file from first argument: ' + process.argv[2]);
 console.log(amdetective(fs.readFileSync(process.argv[2]).toString()));
